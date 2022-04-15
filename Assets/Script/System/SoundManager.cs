41dc89b61de6175868e80ea0e358739bd10c7e31
yosho_cho <choyosho165152987@gmail.com>
2022-04-15 19:05:38 +0900

    SoundManagerをnamespaseに入れた

--- Assets/Script/System/SoundManager.cs
+++ Assets/Script/System/SoundManager.cs
@@ -2,60 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// BGMを鳴らす際のクラス(newして使う)
-/// </summary>
-[System.Serializable]
-public class BGMData 
+namespace GameUtility
 {
-    [SerializeField]
-    public AudioClip Clip;
-    [SerializeField,Range(0,2.0f)]
-    public float Volume = 1.0f;
-}
-
-/// <summary>
-/// SEを鳴らす際のクラス(newして使う)
-/// </summary>
-[System.Serializable]
-public class SEData 
-{
-    [SerializeField]
-    public AudioClip Clip;
-    [SerializeField,Range(0,2.0f)]
-    public float Volume = 1.0f;
-}
-public class SoundManager : SingleMonoBehaviour<SoundManager>
-{
-
-    AudioSource _audioSource;
-
-    protected override void OnAwake()
+    namespace Sound
     {
-        _audioSource = GetComponent<AudioSource>();
-    }
+        /// <summary>
+        /// BGMを鳴らす際のクラス(newして使う)
+        /// </summary>
+        [System.Serializable]
+        public class BGMData
+        {
+            [SerializeField]
+            public AudioClip Clip;
+            [SerializeField, Range(0, 2.0f)]
+            public float Volume = 1.0f;
+        }
 
-    public static void PlayBGM(BGMData data)
-    {
-        if (!Instance._audioSource || !data.Clip) return;
+        /// <summary>
+        /// SEを鳴らす際のクラス(newして使う)
+        /// </summary>
+        [System.Serializable]
+        public class SEData
+        {
+            [SerializeField]
+            public AudioClip Clip;
+            [SerializeField, Range(0, 2.0f)]
+            public float Volume = 1.0f;
+        }
+        public class SoundManager : SingleMonoBehaviour<SoundManager>
+        {
 
-        Instance._audioSource.clip = data.Clip;
-        Instance._audioSource.volume = data.Volume;
-        Instance._audioSource.Play();
-    }
-    public static void PauseBGM()
-    {
-        if (!Instance._audioSource) return;
-        Instance._audioSource.Pause();
-    }
-    public static void UnPauseBGM()
-    {
-        if (!Instance._audioSource) return;
-        Instance._audioSource.UnPause();
-    }
-    public static void PlaySE(SEData data)
-    {
-        if (!Instance._audioSource || !data.Clip) return;
-        Instance._audioSource.PlayOneShot(data.Clip,data.Volume);
+            AudioSource _audioSource;
+
+            protected override void OnAwake()
+            {
+                _audioSource = GetComponent<AudioSource>();
+            }
+
+            public static void PlayBGM(BGMData data)
+            {
+                if (!Instance._audioSource || !data.Clip) return;
+
+                Instance._audioSource.clip = data.Clip;
+                Instance._audioSource.volume = data.Volume;
+                Instance._audioSource.Play();
+            }
+            public static void PauseBGM()
+            {
+                if (!Instance._audioSource) return;
+                Instance._audioSource.Pause();
+            }
+            public static void UnPauseBGM()
+            {
+                if (!Instance._audioSource) return;
+                Instance._audioSource.UnPause();
+            }
+            public static void PlaySE(SEData data)
+            {
+                if (!Instance._audioSource || !data.Clip) return;
+                Instance._audioSource.PlayOneShot(data.Clip, data.Volume);
+            }
+        }
     }
 }
