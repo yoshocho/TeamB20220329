240a140aa6a63240a5bc3b484069cbe3d8269a7e
yosho_cho <choyosho165152987@gmail.com>
2022-06-09 23:58:32 +0900

    プレイヤーが分身を打つ際や分身を拾った時の縮小と拡大処理を追加

--- Assets/Script/Character/Player/Player.cs
+++ Assets/Script/Character/Player/Player.cs
@@ -9,18 +9,29 @@ public sealed class Player : CharacterBase
     float _moveSpeed = 10f;
     [SerializeField]
     float _jumpPower = 10f;
-
-    bool _isGrand = true;
-
+    [SerializeField]
+    GrandChecker _grandChecker;
+    [SerializeField]
+    float _magnification = 0.1f;
+    [SerializeField]
+    float _maxMagnification = 3.0f;
+    [SerializeField]
+    float _minimumMagnification = 0.5f;
+    
+    Vector3 _maxSize;
+    Vector3 _minimumSize;
     Animator _anim;
     Rigidbody2D _rb;
     SpriteRenderer _charaImage;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        if(!_grandChecker) _grandChecker = GetComponentInChildren<GrandChecker>();
         _charaImage = GetComponentInChildren<SpriteRenderer>();
         _anim = GetComponentInChildren<Animator>();
         _status.SetUp(_status.hp);
+        _maxSize = new Vector3(_maxMagnification, _maxMagnification, 1.0f);
+        _minimumSize = new Vector3(_minimumMagnification,_minimumMagnification,1.0f);
     }
 
     void Update()
@@ -28,7 +39,14 @@ public sealed class Player : CharacterBase
 
         ApplyMove();
 
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrand)
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            Debug.Log("shoot");
+            CreateClone();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrand())
         {
             _anim.SetBool("Jump", true);
             Jump();
@@ -42,14 +60,14 @@ public sealed class Player : CharacterBase
         if (Input.GetKey(KeyCode.A))
         {
             _charaImage.flipX = true;
-            if (_isGrand) _anim.SetBool("Move", true);
+            if (IsGrand()) _anim.SetBool("Move", true);
             else _anim.SetBool("Move", false);
             _rb.velocity = new Vector2(-_moveSpeed, _rb.velocity.y);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             _charaImage.flipX = false;
-            if (_isGrand) _anim.SetBool("Move", true);
+            if (IsGrand()) _anim.SetBool("Move", true);
             else _anim.SetBool("Move", false);
             _rb.velocity = new Vector2(_moveSpeed, _rb.velocity.y);
         }
@@ -59,23 +77,38 @@ public sealed class Player : CharacterBase
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit");
+        if (!collision.collider.CompareTag("Clone")) return;
+        Debug.Log("CloneÇ…Hit");
+        if (_maxSize.x <= transform.localScale.x || _maxSize.y <= transform.localScale.y) 
+        {
+            transform.localScale = _maxSize;
+            Debug.Log("ç≈ëÂSize");
+            return;
+        }
+        Debug.Log("ëÂÇ´Ç≠Ç»ÇÈ");
+        transform.localScale += new Vector3(_magnification,_magnification,0.0f);
+    }
     void Jump() => _rb.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
 
+    void CreateClone()
+    {
+        Debug.Log("Create");
+        if (_minimumSize.x >= transform.localScale.x || _minimumSize.y >= transform.localScale.y)
+        {
+            transform.localScale = _minimumSize;
+            Debug.Log("ç≈è¨Size");
+            return;
+        }
+        Debug.Log("è¨Ç≥Ç≠Ç»ÇÈ");
+        transform.localScale -= new Vector3(_magnification,_magnification,0.0f);
+    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public bool IsGrand()
     {
-        if (collision.gameObject.tag != "Grand") return;
-        _isGrand = true;
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag != "Grand") return;
-        _isGrand = false;
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag != "Grand") return;
-        _isGrand = true;
+        return _grandChecker.IsGrand;
     }
 
     public override void GetDamage(int damage)
