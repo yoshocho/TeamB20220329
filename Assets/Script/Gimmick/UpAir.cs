f5d4774a20ef0d35c4630d1811932bcdaa44adc4
yosho_cho <choyosho165152987@gmail.com>
2022-10-17 19:42:36 +0900

    上昇気流ギミックを修正

--- Assets/Script/Gimmick/UpAir.cs
+++ Assets/Script/Gimmick/UpAir.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class UpAir : GimmickBase
 {
     [SerializeField]
     float _upSpeed = 2.0f;
+    [SerializeField]
+    Transform _targetPos;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -15,6 +18,17 @@ public class UpAir : GimmickBase
         {
             Debug.Log("aru");
             rb.RB.AddForce(Vector2.up * _upSpeed, ForceMode2D.Impulse);
+            //rb.RB.DOMoveY(_targetPos.position.y,1f,true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IRBCtrl rb;
+        if (collision.TryGetComponent(out rb))
+        {
+            Debug.Log("aru");
+            rb.RB.velocity = new Vector2(rb.RB.velocity.x, 0.0f);
+            rb.RB.AddForce(Vector2.up * 0.3f, ForceMode2D.Impulse);
         }
     }
 }
